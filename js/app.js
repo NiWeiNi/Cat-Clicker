5ec8cc41e4eb36e4e5b157e1dc4d2dfc5599e56d
Wei <weinini007@gmail.com>
2018-04-30 19:00:22 +0200

    Add event listener to big cat picture

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
-"use strict"
 
-const firstCat = document.querySelector(".first-cat");
-const secondCat = document.querySelector(".second-cat");
-let numberClicks = 0;
-firstCat.addEventListener("click", () => { numberClicks += 1; })
-secondCat.addEventListener("click", () => { numberClicks += 1; })
+
+(function() {
+    "use strict"
+
+    // Variables
+    const pictureCat = document.querySelector(".picture-cat");
+    const listCats = document.querySelectorAll("li");
+    let numberClicks = 0;
+    let clicksTitle = document.querySelector(".number-clicks");
+
+    // Event listener on list
+    for (let i = 0; i < listCats.length; i++) {
+        listCats[i].addEventListener("click", () => { numberClicks += 1;
+            clicksTitle.textContent = numberClicks; })
+    }
+
+    // Event listener on picture
+    pictureCat.addEventListener("click", () => { numberClicks += 1;
+        clicksTitle.textContent = numberClicks; })
+})();
+
