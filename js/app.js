711a0bad70b2e017dcbefc9cb223696d634af8d0
Wei <weinini007@gmail.com>
2018-04-30 19:20:41 +0200

    Change cat pictures names

--- js/app.js
+++ js/app.js
@@ -5,14 +5,17 @@
 
     // Variables
     const pictureCat = document.querySelector(".picture-cat");
+    const nameCat = document.querySelector(".title-name");
     const listCats = document.querySelectorAll("li");
     let numberClicks = 0;
     let clicksTitle = document.querySelector(".number-clicks");
 
     // Event listener on list
     for (let i = 0; i < listCats.length; i++) {
-        listCats[i].addEventListener("click", () => { numberClicks += 1;
-            clicksTitle.textContent = numberClicks; })
+        listCats[i].addEventListener("click", (e) => { 
+            nameCat.textContent = e.target.dataset.title;
+            pictureCat.src =  e.target.dataset.src;
+        })
     }
 
     // Event listener on picture
